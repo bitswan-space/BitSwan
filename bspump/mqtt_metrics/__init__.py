d6aaf3514946f0b8f1a1162bc4060dbfd4c641ac
Lukas Vecerka <vecerkalukas18@gmail.com>
2023-09-14 18:38:05 +0200

    add sub to topic based on container_id, parsing the topology of pipelines

--- bspump/mqtt_metrics/__init__.py
+++ bspump/mqtt_metrics/__init__.py
@@ -2,13 +2,43 @@ import paho.mqtt.client as mqtt
 import time
 import json
 from datetime import datetime
+import logging
 from asab.web.rest.json import JSONDumper
+
+L = logging.getLogger(__name__)
+
+
+def parse_topology(pipelines: dict):
+
+    components = []
+    for pipeline in pipelines.values():
+        components.append(pipeline["Sources"][0])
+        for processor in pipeline["Processors"][0]:
+            components.append(processor)
+
+    print(components)
+    output_list = []
+    for i in range(len(components)):
+        current_dict = {"id": components[i]["Id"]}
+
+        # If it's not the last element, add the "wires" key
+        if i < len(components) - 1:
+            current_dict["wires"] = [components[i + 1]["Id"]]
+        else:
+            current_dict["wires"] = []
+
+        output_list.append(current_dict)
+
+    output = {"topology": output_list}
+    return output
+
 
 
 container_id = None
 # Callback when a message is received
 def on_message_factory(app):
-    dumper = JSONDumper(pretty=True)
+    dumper = JSONDumper(pretty=False)
+
     def on_message(client, userdata, message):
         global container_id
         payload = message.payload.decode("utf-8")
@@ -16,10 +46,12 @@ def on_message_factory(app):
 
         if payload == "get":
             svc = app.get_service("bspump.PumpService")
-            metrics_and_topology = dumper(svc.Pipelines)
 
-            client.publish(f"{container_id}/Metrics", json.dumps(metrics_and_topology))
+            topology = parse_topology(json.loads(dumper(svc.Pipelines)))
+            L.warning(f"Publishing to {container_id}/Metrics")
+            client.publish(f"{container_id[:20]}/Metrics", json.dumps(topology))
     return on_message
+
 
 # Callback when connected to the MQTT broker
 def on_connect(client, userdata, flags, rc):
@@ -32,7 +64,9 @@ def on_connect(client, userdata, flags, rc):
                 break
         except FileNotFoundError:
             time.sleep(1)
-    client.subscribe(f"{container_id}/Metrics/get")
+    # L.warning(f"Subscribing to {container_id}/Metrics/get")
+    client.subscribe(f"{container_id[:20]}/Metrics/get")
+
 
 def initialize_mqtt(app, broker):
     # Initialize MQTT client
