73e234c7cb5841cba1a2aa051230bdb79f846bb7
Timothy Hobbs <timothy@hobbs.cz>
2024-07-30 20:30:38 +0200

    Add webserver pipelines

---
+++ bspump/http/web/server.py
@@ -0,0 +1,96 @@
+import logging
+import traceback
+import asyncio
+
+from ...abc.source import Source
+from ...abc.sink import Sink
+from ...abc.connection import Connection
+
+import aiohttp.web
+#
+
+L = logging.getLogger(__name__)
+
+class WebServerConnection(Connection):
+    """
+    Source with events from a specific route.
+    """
+
+    ConfigDefaults = {
+        "port": 8080,
+        "max_body_size_bytes": 1024 * 1024 * 1000,
+    }
+
+
+    def __init__(self, app, id=None, config=None):
+        super().__init__(app, id=id, config=config)
+
+        self.aiohttp_app = aiohttp.web.Application(
+            client_max_size=self.Config["max_body_size_bytes"]
+        )
+        self.start_server()
+
+    def start_server(self):
+        print("Starting webserver")
+        try:
+            self.App.Loop.create_task(
+                aiohttp.web._run_app(
+                    self.aiohttp_app,
+                    port=self.Config["port"],
+                )
+            )
+        except Exception as e:
+            print("Exception: {}".format(e))
+            import traceback
+            traceback.print_exc()
+
+
+
+class WebRouteSource(Source):
+    """
+    WebSource is a source that listens on a specified port and serves HTTP requests.
+    """
+
+    ConfigDefaults = {
+        "port": "8080",
+        "max_body_size_bytes": 1024 * 1024 * 1000,
+    }
+
+    def __init__(self, app, pipeline, connection, method, route, id=None, config=None):
+        print("WebRouteSource")
+        super().__init__(app, pipeline, id=id, config=config)
+
+        self.Connection = pipeline.locate_connection(app, connection)
+        self.aiohttp_app = self.Connection.aiohttp_app
+        self.aiohttp_app.router.add_route(method, route, self.handle_request)
+
+    async def main(self):
+        pass
+
+    async def handle_request(self, request):
+        try:
+            response_future = asyncio.Future()
+            await self.process({
+                "request": request,
+                "response_future": response_future,
+                "status": 200,
+            })
+            return await response_future
+        except Exception as e:
+            L.exception("Exception in WebSource")
+            return aiohttp.web.Response(status=500)
+
+class WebSink(Sink):
+    """
+    WebSink is a sink that sends HTTP requests.
+    """
+    def process(self, context, event):
+        event["response_future"].set_result(aiohttp.web.Response(status=event["status"], text=event["response"]))
+
+class JSONWebSink(Sink):
+    """
+    JSONWebSink is a sink that sends HTTP requests with JSON content.
+    """
+
+    def process(self, context, event):
+        event["response_future"].set_result(aiohttp.web.json_response(event["response"], status=event["status"]))
