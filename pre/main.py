35b824f19051e000be267294d9f486f361442f00
Timothy Hobbs <timothy@hobbs.cz>
2024-07-31 17:15:47 +0200

    Don't totally bork out on exceptions

--- pre/main.py
+++ pre/main.py
@@ -9,7 +9,9 @@ notebook_path = os.environ.get("JUPYTER_NOTEBOOK", "pipelines/main.ipynb")
 if os.path.exists(notebook_path):
   with open(notebook_path) as nb:
       notebook = json.load(nb)
+      cell_number = 0
       for cell in notebook["cells"]:
+          cell_number += 1
           if cell["cell_type"] == "code":
               source = cell["source"]
               if len(source) > 0 and "#ignore" not in source[0]:
@@ -19,7 +21,13 @@ if os.path.exists(notebook_path):
                       if line.startswith("!"):
                           continue
                       clean_code += line + "\n"
-                  exec(clean_code)
+                  try:
+                      exec(clean_code)
+                  except Exception as e:
+                      print(f"Error in cell: {cell_number}\n{clean_code}")
+                      # print traceback
+                      import traceback
+                      traceback.print_exc()
 else:
     print(f"Notebook {notebook_path} not found")
                   
