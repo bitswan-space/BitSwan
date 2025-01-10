03816b40048a2debeeff7f01aba812a473d3ecc8
Matej Slivka <matej.slivka@wingsdata.ai>
2025-01-10 17:20:52 +0100

    Adding Dockerfile for automatic creation of mkdocs on port 8080

---
+++ docserver/build_and_push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get Year and Commit Hash
+YEAR=$(date +%Y)
+COMMIT_HASH=$(git rev-parse --short HEAD)
+IMAGE_TAG="bitswan/docserver"
+
+cd docserver
+
+# Build and push Docker images
+docker build -t $IMAGE_TAG:latest -t $IMAGE_TAG:$YEAR-${GITHUB_RUN_ID}-git-$COMMIT_HASH -f ./Dockerfile .
+
+docker push $IMAGE_TAG:latest
+docker push $IMAGE_TAG:$YEAR-${GITHUB_RUN_ID}-git-$COMMIT_HASH
+
+# Push a tag with the image ID
+IMAGE_ID=$(docker images --no-trunc -q $IMAGE_TAG:latest | sed 's/:/_/g')
+docker tag $IMAGE_TAG:latest $IMAGE_TAG:$IMAGE_ID
+docker push $IMAGE_TAG:$IMAGE_ID
