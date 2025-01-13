768ec431ab028dc9ca88c316a2e7e8396d28c225
Jan Kotrc <john.kotrc@gmail.com>
2025-01-13 12:25:08 +0100

    autopipeline imports from temporary python file

--- bspump/main.py
+++ bspump/main.py
@@ -1,15 +1,16 @@
 import json
 import os
 import ast
-import asyncio
-
-config = None
-__bitswan_dev = False
-# cls._cell_code_contents: dict[int, str] = {}
-__bs_step_locals = {}
+import sys
+import tempfile
 
 from bspump.jupyter import *  # noqa: F403
 import bspump.jupyter
+
+config = None
+__bitswan_dev = False
+__bs_step_locals = {}
+
 
 def contains_function_call(ast_tree, function_name):
     for node in ast.walk(ast_tree):
@@ -17,6 +18,7 @@ def contains_function_call(ast_tree, function_name):
             if isinstance(node.func, ast.Name) and node.func.id == function_name:
                 return True
     return False
+
 
 class NotebookParser:
     _in_autopipeline = False
@@ -39,7 +41,7 @@ class NotebookParser:
                     continue
             lines_out = []
             for i in range(len(lines)):
-                _indent = ("    " if i in indent_lines else "")
+                _indent = "    " if i in indent_lines else ""
                 lines_out.append(_indent + lines[i])
             # return "\n".join([f"    {line}" for line in lines]) + "\n\n"
             return "\n".join(lines_out) + "\n\n"
@@ -63,8 +65,12 @@ class NotebookParser:
                 if not cls._in_autopipeline:
                     fout.write(cell_str(clean_code))
                 else:
-                    cls._cell_processor_contents[cls._cell_number] = cell_str(clean_code, True)
-                if not cls._in_autopipeline and contains_function_call(ast.parse(clean_code), "auto_pipeline"):
+                    cls._cell_processor_contents[cls._cell_number] = cell_str(
+                        clean_code, True
+                    )
+                if not cls._in_autopipeline and contains_function_call(
+                    ast.parse(clean_code), "auto_pipeline"
+                ):
                     cls._in_autopipeline = True
 
     @classmethod
@@ -77,10 +83,7 @@ class NotebookParser:
                 cls._cell_number += 1
                 cls.parse_cell(cell, f)
             # for cell_number, pcell in cls._cell_processor_contents:
-            print(f"PROCESSOR CONTENTS {cls._cell_processor_contents}")
-            j = ''.join(list(cls._cell_processor_contents.values()))
-            print(f"JOIN {j}")
-
+            # j = ''.join(list(cls._cell_processor_contents.values()))
 
             step_func_code = f"""@async_step
 async def processor_internal(inject, event):
@@ -90,29 +93,31 @@ async def processor_internal(inject, event):
             f.write(step_func_code)
 
 
-
-            
-
-
 def main():
     app = App()  # noqa: F405
     if app.Test:
         bspump.jupyter.bitswan_test_mode.append(True)
 
-    if os.path.exists(app.Notebook):
-        with open(app.Notebook) as nb:
-            notebook = json.load(nb)
-            NotebookParser.parse_notebook(notebook)
-            from tmp import app
-    else:
-        print(f"Notebook {app.Notebook} not found")
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        if os.path.exists(app.Notebook):
+            with open(app.Notebook) as nb:
+                notebook = json.load(nb)
+                NotebookParser.parse_notebook(
+                    notebook, out_path=f"{tmpdirname}/autopipeline_tmp.py"
+                )
+                sys.path.insert(0, tmpdirname)
+                tmp_module = __import__("autopipeline_tmp")
+        else:
+            print(f"Notebook {app.Notebook} not found")
 
-    if bspump.jupyter.bitswan_auto_pipeline.get("sink") is not None:
-        register_sink(bspump.jupyter.bitswan_auto_pipeline.get("sink"))  # noqa: F405
-        end_pipeline()  # noqa: F405
+        if bspump.jupyter.bitswan_auto_pipeline.get("sink") is not None:
+            register_sink(
+                bspump.jupyter.bitswan_auto_pipeline.get("sink")
+            )  # noqa: F405
+            end_pipeline()  # noqa: F405
 
-    app.init_componets()
-    app.run()
+        app.init_componets()
+        app.run()
 
 
 if __name__ == "__main__":
