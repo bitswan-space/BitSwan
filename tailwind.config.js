0c2eb7e898918e4225e7ffb645aa157fe770326a
monnikareh <monikarehakova333@gmail.com>
2025-01-15 13:20:12 +0100

    feat(WebServer): add tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './bspump/**/*.py',
+    './static/**/*.js',
+    './static/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
