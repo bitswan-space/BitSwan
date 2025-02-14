972dab15d90aadfe4afad716d12a5632174e56f7
monnikareh <monikarehakova333@gmail.com>
2025-02-14 12:46:54 +0100

    feat: float field

--- bspump/http/web/server.py
+++ bspump/http/web/server.py
@@ -327,7 +327,7 @@ class IntField(Field):
         if not default:
             default = 0
 
-        template = env.get_template("int-field.html")
+        template = env.get_template("number-field.html")
         return template.render(
             default=default,
             default_input_props=self.default_input_props,
@@ -343,11 +343,13 @@ class FloatField(Field):
     def inner_html(self, default=0, readonly=False):
         if not default:
             default = 0.0
-        return f"""
-        <div class="mt-1">
-            <input type="number" value="{default}" class="{self.default_classes}" {self.default_input_props}>
-        </div>
-        """
+
+        template = env.get_template("number-field.html")
+        return template.render(
+            default=default,
+            default_input_props=self.default_input_props,
+            default_classes=self.default_classes
+        )
 
     def clean(self, data, request: Request | None = None):
         if type(data.get(self.name)) == str:
