880a2dccc093854a3425e6a93c96cbbc702b9a2e
monnikareh <monikarehakova333@gmail.com>
2025-02-14 12:53:26 +0100

    feat: raw json field

--- bspump/http/web/server.py
+++ bspump/http/web/server.py
@@ -382,11 +382,12 @@ class FileField(Field):
 
 class RawJSONField(Field):
     def inner_html(self, default="", readonly=False):
-        return f"""
-      <div class="mt-1">
-      <textarea class="{self.default_classes}" {self.default_input_props}>{default}</textarea>
-      </div>
-      """
+        template = env.get_template("raw-json_field.html")
+        return template.render(
+            default=default,
+            default_input_props=self.default_input_props,
+            default_classes=self.default_classes,
+        )
 
     def clean(self, data, request: Request | None = None):
         if type(data.get(self.name)) == str:
