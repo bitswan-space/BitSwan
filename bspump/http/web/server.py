43142cfc4d0a4aafcf4438390826a06bd043df8b
monnikareh <monikarehakova333@gmail.com>
2025-02-14 12:21:51 +0100

    feat:text field

--- bspump/http/web/server.py
+++ bspump/http/web/server.py
@@ -277,11 +277,12 @@ class Field(BaseField):
 
 class TextField(Field):
     def inner_html(self, default="", readonly=False):
-        return f"""
-        <div class="mt-1">
-            <input type="text" class="{self.default_classes}" value="{default}" {self.default_input_props}>
-        </div>
-        """
+        template = env.get_template("text-field.html")
+        return template.render(
+            default=default,
+            default_classes=self.default_classes,
+            default_input_props=self.default_input_props,
+        )
 
 
 class ChoiceField(Field):
