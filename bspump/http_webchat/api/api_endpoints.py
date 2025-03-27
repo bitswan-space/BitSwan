fc53423fd0a20e036581666c0b09670c1df670a1
monnikareh <monikarehakova333@gmail.com>
2025-03-27 17:07:34 +0100

    feat: first example fund

--- bspump/http_webchat/api/api_endpoints.py
+++ bspump/http_webchat/api/api_endpoints.py
@@ -22,7 +22,25 @@ async def get_web_chat_response(request):
     if not fund_id:
         return aiohttp.web.json_response({"error": "Missing fund_id"}, status=400)
 
-    webchat = WebChatResponse(response_text=f"Calculating odkupy for Fund {fund_id}")
+    if fund_id == "123":
+        return await get_response_123()
+    elif fund_id == "12":
+        return await get_response_12()
+    else:
+        webchat = WebChatResponse(response_text=f"Calculating odkupy for Fund {fund_id}")
+        rendered_html = webchat.render_response(template_env)
+        return aiohttp.web.Response(text=rendered_html, content_type='text/html')
+
+async def get_response_123():
+    calculating = WebChatResponse(response_text=f"Calculating odkupy for Fund {123}")
+    result = WebChatResponse(response_text=f"Total valuation is 2 300 345 CZK.")
+    next_message = WebChatResponse(response_text=f"Please pick another fund for calculation.")
+
+    rendered_html = calculating.render_response(template_env) + result.render_response(template_env) + next_message.render_response(template_env)
+    return aiohttp.web.Response(text=rendered_html, content_type='text/html')
+
+async def get_response_12():
+    webchat = WebChatResponse(response_text=f"Calculating odkupy for Fund {12}")
     rendered_html = webchat.render_response(template_env)
     return aiohttp.web.Response(text=rendered_html, content_type='text/html')
 
@@ -38,5 +56,4 @@ async def get_fund_info(request):
         "name": "Sample Fund",
         "balance": 100000,
     }
-
     return aiohttp.web.json_response(fund_info)
