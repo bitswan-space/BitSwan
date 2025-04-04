c9dca0b77036a8d5b360813895ed151dbccda9c8
monnikareh <monikarehakova333@gmail.com>
2025-04-04 10:50:34 +0200

    feat: create webchat class

--- bspump/http_webchat/api/app.py
+++ bspump/http_webchat/api/app.py
@@ -3,14 +3,12 @@ import aiohttp.web
 from bspump.http_webchat.api.api_endpoints import get_fund_info, get_welcome_message, get_prompt_input, \
     get_web_chat_response
 
-from bspump.http_webchat.server.app import app, set_app
+from bspump.http_webchat.server.app import app, set_app, WebChat
 
 if __name__ == "__main__":
-    set_app()
-    app.add_routes([
-        aiohttp.web.get("/api/fund", get_fund_info),
-        aiohttp.web.get("/api/welcome_message", get_welcome_message),
-        aiohttp.web.get("/api/prompt_input", get_prompt_input),
-        aiohttp.web.get("/api/response_box", get_web_chat_response)
-    ])
-    aiohttp.web.run_app(app, host="127.0.0.1", port=8080)
+    webchat = WebChat({
+        "/api/fund" : get_fund_info,
+        "/api/welcome_message" : get_welcome_message,
+        "/api/prompt_input" : get_prompt_input,
+        "/api/response_box" : get_web_chat_response
+    })
