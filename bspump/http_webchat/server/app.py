556b4cc5618d4cb402d1f7692af0424d1a0ccb59
monnikareh <monikarehakova333@gmail.com>
2025-04-04 14:10:32 +0200

    feat: align response to the right

--- bspump/http_webchat/server/app.py
+++ bspump/http_webchat/server/app.py
@@ -60,16 +60,13 @@ class WebchatPrompt:
     def get_html(self):
         return self.input_html
 
-
 class WebChatResponse:
     def __init__(self, input_html=None):
         self.input_html = input_html or ""
-        self.styling = "bg-white shadow-md rounded-xl rounded-tr-none w-[90%] px-8 py-6 flex items-center border border-gray-300"
 
     def get_context(self):
         return {
             'response_text': self.input_html,
-            'response_styling': self.styling
         }
 
     def get_html(self, template_env):
