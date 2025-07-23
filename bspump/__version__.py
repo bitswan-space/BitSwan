56cfa6dd849ed049cb4c2d089fa36d13d2ee60d6
Timothy Hobbs <timothy@hobbs.cz>
2025-07-23 12:45:17 +0200

    Autorelease bitswan try #2

--- bspump/__version__.py
+++ bspump/__version__.py
@@ -1,12 +1,10 @@
 # THIS-FILE-WILL-BE-REPLACED !!! DO NOT CHANGE OR MODIFY THIS FILE !!!
 
-# During `setup.py build_py`, this file is overwritten
-# __version__ = '[git describe --abbrev=7 --tags --dirty=+dirty --always]
-# __build__ = '[git rev-parse HEAD]'
+# During the GitHub release workflow, this file is overwritten with the CalVer version
+# For development, this defaults to "devel"
 
 # PEP 440 -- Version Identification and Dependency Specification
 # https://www.python.org/dev/peps/pep-0440/
 
-
-__version__ = "devel"
-__build__ = "devel"
+__version__ = "0.0.0.dev"
+__build__ = "0.0.0.dev"
